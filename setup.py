8d2c3660606d656c6ab7af463bdd0ea93d6dc3cf
network-eyu <networkeyuel@duck.com>
2024-06-29 17:37:42 +0300

    update to 0.1.11
    Signed-off-by: network-eyu <networkeyuel@duck.com>

--- setup.py
+++ setup.py
@@ -9,21 +9,21 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 VERSION = '0.1.11'
-DESCRIPTION = 'Telebirr integration'
-LONG_DESCRIPTION = 'This package is a helper package with telebirr integration.'
+DESCRIPTION = 'NetworkEyuel integration'
+LONG_DESCRIPTION = 'This package is a helper package with NetworkEyuel integration.'
 
 # Setting up
 setup(
-    name="telebirr",
+    name="networkeyuel",
     version=VERSION,
-    author="Eba Alemayehu",
-    author_email="<ebaalemayhu3@gmail.com>",
+    author="network-eyu",
+    author_email="<networkeyuel@duck.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
     install_requires=['pycryptodome', 'requests', 'cryptography'],
-    keywords=['python', 'telebirr', 'payment', 'ethiopia', 'ethio telecom'],
+    keywords=['python', 'networkeyuel', 'payment', 'ethiopia', 'ethio telecom'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
